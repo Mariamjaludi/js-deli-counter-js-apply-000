54d219fc76a58c8dcb4897c47f5b68ca7b2f9032
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-21 18:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 function takeANumber(currentLine, newCustomer){
   var temp = currentLine.push(newCustomer);
   var welcome = `Welcome, ${newCustomer}. You are number ${temp.length} in line.`;
-  return temp;
+  return welcome;
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    var temp = katzDeliLine[0];
+    var sentence = `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
+    return sentence;
+  }
+}
+function currentLine(currentLine){
+    if (currentLine.length != 0){
+      var sentence = "The line is currently: ";
+      for (var i=0; i < currentLine.length; i++){
+        var temp = i + 1;
+        if (currentLine.length === 1 || i === currentLine.length -1){
+          sentence = sentence + `${temp}. ${currentLine[i]}`
+        }
+        else{
+          sentence = sentence + `${temp}. ${currentLine[i]}, `;
+        }
+      }
+      return sentence;
+    }
+    else return "The line is currently empty."
+  }
